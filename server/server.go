c60a516bd2136442c46c829b759dc0ceda06831b
Mehmet Emin Gelmedi <mehmetemingelmedi@gmail.com>
2019-03-12 21:51:42 +0300

    ADD JWT Auth

--- server/server.go
+++ server/server.go
@@ -3,10 +3,62 @@ package server
 import (
 	"gonews/handlers"
 	"os"
+	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+func login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	// Throws unauthorized error
+	if username != "jon" || password != "shhh!" {
+		return echo.ErrUnauthorized
+	}
+
+	// Set custom claims
+	claims := &jwtCustomClaims{
+		"Jon Snow",
+		true,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
+
+func accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome to Gonews API powered by NewsAPI")
+}
+
+func restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	//name := claims.Name
+	return c.JSON(http.StatusOK, claims)
+}
 
 func Run() {
 	// Echo instance
@@ -22,13 +74,27 @@ func Run() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-		AllowMethods: []string{echo.GET},
+		AllowMethods: []string{echo.GET, echo.POST},
 	}))
 
 	/*** Routes ***/
 	// News Routes
-	e.GET("/news", handlers.GetAllNews)
-	e.GET("/news/:search", handlers.GetNewsBySearch)
+	e.POST("/login", login)
+	e.GET("/", accessible)
+
+	r := e.Group("/api")
+
+	secret := os.Getenv("SECRET")
+	config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte(secret),
+	}
+
+	r.Use(middleware.JWTWithConfig(config))
+	r.GET("", restricted)
+
+	r.GET("/news", handlers.GetAllNews)
+	r.GET("/news/:search", handlers.GetNewsBySearch)
 
 	// Start server
 	serverPort := os.Getenv("SERVER_PORT")
